b471ec5b73350479ce9b4bb451ef918e238f4252
PallaDeepika <palladeepika245@gmail.com>
2021-11-16 10:31:20 +0530

    initial commit

---
+++ src/basics/FormJava.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class FormJava {
+    public static void main(String[] args) {
+        Student deepu = new Student("luckypriya",123,876);
+        System.out.println(deepu.getName());
+    }
+}
+
---
+++ src/basics/Loops.kt
@@ -0,0 +1,11 @@
+package basics
+
+class Loops {
+}
+
+fun main() {
+    val items = listOf("apple", "mango", "grapes")
+    for (item in items) {
+        println(item)
+    }
+}
